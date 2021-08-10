f416d584c8b2dc70009811800f96ca4245c188d2
lucasspierre <lucaspierrealmeida@gmail.com>
2021-08-10 16:34:23 -0300

    1º versão

---
+++ Readme.md
@@ -0,0 +1,4 @@
+<h2>Uma automação simples utilizando duas bibliotecas incríveis do Python</h2>
+<b>Pandas</b> - Capaz de fazer a leitura e extração de dados de diversos arquivos;
+<br>
+<b>Pyautogui</b> - Transforma as funções de um teclado e mouse em códigos podendo automatizar uma infinidade de processos diários.
---
+++ automation.py
@@ -0,0 +1,37 @@
+import pyautogui
+import pyperclip
+
+#Exportando dados
+import pandas as pd
+df = pd.read_excel(r'PyAutoGui/Example.xlsx') #Faz a leitura do arquivo excel (xlsx)
+faturamento = df['Valor Final'].sum() #Faz a leitura da coluna 'Valor Final' e faz a soma de todos os valores dos produtos
+qtde_produtos = df['Quantidade'].sum() #Faz a leitura da coluna 'Quantidade' e soma todos os produtos vendidos
+
+pyautogui.PAUSE = 1 #Delay de 1 segundo para cada ação
+pyautogui.press('win')
+pyautogui.write('google chrome')
+pyautogui.press('enter')
+pyautogui.write("mail.google.com")
+pyautogui.press('enter')
+pyautogui.sleep(2)
+pyautogui.click(x=71, y=174)
+pyautogui.write('lucaspierrealmeida@gmail.com')
+pyautogui.press('tab')
+pyautogui.press('tab')
+assunto = "Relatório mensal de vendas"
+pyperclip.copy(assunto)
+pyautogui.hotkey('ctrl','v')
+pyautogui.press('tab')
+texto = f"""
+Prezados, bom dia
+Segue abaixo o Relatório total de vendas mensais
+
+O faturamento foi de: R$ {faturamento:,.2f}
+e foram vendidos {qtde_produtos:,} produtos
+
+Att
+Lucas Pierre
+"""
+pyperclip.copy(texto)
+pyautogui.hotkey('ctrl','v')
+pyautogui.hotkey('ctrl','enter')
